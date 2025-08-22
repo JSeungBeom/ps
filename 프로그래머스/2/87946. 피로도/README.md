5b40d9d2ffc5d18985ed8d3254319387f38c0322
hmmmman <99180728+JSeungBeom@users.noreply.github.com>
2025-08-22 14:18:00 +0900

    [level 2] Title: 피로도, Time: 14.30 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/README.md
+++ 프로그래머스/2/87946. 피로도/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피로도 - 87946 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946) 
 
 ### 성능 요약
 
-메모리: 98.1 MB, 시간: 138.16 ms
+메모리: 80 MB, 시간: 14.30 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 17일 17:49:50
+2025년 08월 22일 14:17:56
 
 ### 문제 설명
 
--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,51 +1,52 @@
 import java.util.*;
 
 class Solution {
-    static int n;
-    static boolean[] vis;
-    static int[] arr;
-    static int mx;
     
-    public int solution(int k, int[][] dungeons) {
-        n = dungeons.length;
-        vis = new boolean[n];
-        arr = new int[n];
-
-        backtracking(0, k, dungeons);
+    static int[] arr;
+    static boolean[] vis;
+    static int answer;
+    
+    public int solution(int k, int[][] dungeons) {        
+        int n = dungeons.length;
         
-        return mx;
+        arr = new int[n];
+        vis = new boolean[n];
+        
+        perm(0, n, k, dungeons);
+        
+        
+        return answer;
     }
     
-    static void backtracking(int k, int cur, int[][] dungeons) {
-        if(mx == n)
-            return;
-        if(n == k){
-            int cnt = 0;
-            int now = cur;
-            for(int i = 0; i < n; i++){
-                
-                if(dungeons[arr[i]][0] <= now) {
-                    now -= dungeons[arr[i]][1]; cnt++;
-                }
-            }
-            
-            System.out.println();
-            
-            mx = Math.max(cnt, mx);
-            
-            
+    static void perm(int n, int k, int rest, int[][] dungeons) {
+        if(n == k) {
+            explore(k, rest, dungeons);
             return;
         }
         
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < k; i++) {
             if(vis[i]) continue;
             
-            arr[k] = i;
             vis[i] = true;
-            
-            backtracking(k + 1, cur, dungeons);
-            
+            arr[n] = i;
+            perm(n + 1, k, rest, dungeons);
             vis[i] = false;
         }
+    }
+    
+    static void explore(int n, int rest, int[][] dungeons) {
+        int cnt = 0;
+        
+        for(int i = 0; i < n; i++) {
+            int min = dungeons[arr[i]][0];
+            int consume = dungeons[arr[i]][1];
+            
+            if(rest >= min) {
+                rest -= consume;
+                cnt++;
+            }
+        }
+        
+        answer = Math.max(cnt, answer);
     }
 }
