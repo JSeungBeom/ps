7f438e927d50ff77226ffbaa097b65dae7447566
hmmmman <99180728+JSeungBeom@users.noreply.github.com>
2025-10-18 09:25:15 +0900

    [Gold III] Title: 사회망 서비스(SNS), Time: 2412 ms, Memory: 424636 KB -BaekjoonHub

---
+++ 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+
+public class Main {
+
+	static int N;
+	static List<List<Integer>> adj = new ArrayList<>();
+	static int[][] dp;
+	static boolean[] vis;
+	
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i <= N; i++) {
+			adj.add(new ArrayList<>());
+		}
+		
+		dp = new int[N + 1][2];
+		vis = new boolean[N + 1];
+		
+		for(int i = 0; i < N - 1; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			
+			adj.get(u).add(v);
+			adj.get(v).add(u);
+		}
+		
+		dfs(1);
+		
+		System.out.println(Math.min(dp[1][0], dp[1][1]));
+	}
+	
+	static void dfs(int cur) {
+		vis[cur] = true;
+		dp[cur][0] = 0;
+		dp[cur][1] = 1;
+		
+		for(int nxt : adj.get(cur)) {
+			if(vis[nxt]) continue;
+			
+			dfs(nxt);
+			
+			dp[cur][0] += dp[nxt][1];
+			dp[cur][1] += Math.min(dp[nxt][0], dp[nxt][1]);
+		}
+	}
+	
+
+}
