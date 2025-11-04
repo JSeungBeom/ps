6edecde77172519c5eddcd8e6e7ccc4cba6c0e25
hmmmman <99180728+JSeungBeom@users.noreply.github.com>
2025-11-04 23:57:30 +0900

    [Gold V] Title: 조 짜기, Time: 136 ms, Memory: 14472 KB -BaekjoonHub

---
+++ 백준/Gold/2229. 조 짜기/조 짜기.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static int N;
+	static int[] arr;
+	static int[] dp;
+	
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		N = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		arr = new int[N + 1];
+		dp = new int[N + 1];
+		
+		for(int i = 1; i <= N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		dp[1] = 0;
+		
+		for(int i = 1; i <= N; i++) {
+			int max = arr[i];
+			int min = arr[i];
+			
+			for(int j = i; j >= 1; j--) {
+				max = Math.max(max, arr[j]);
+				min = Math.min(min, arr[j]);
+				
+				dp[i] = Math.max(dp[i], dp[j - 1] + max - min);
+			}
+			
+		}
+		
+		System.out.println(dp[N]);
+	}
+
+}
