6e59bafe9082e105163156dea4c3af6fd35abad7
hmmmman <99180728+JSeungBeom@users.noreply.github.com>
2025-11-14 17:40:14 +0900

    [Gold II] Title: 저울, Time: 108 ms, Memory: 14636 KB -BaekjoonHub

---
+++ 백준/Gold/2437. 저울/저울.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static int N;
+	static int[] choo;
+	
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        
+        choo = new int[N];
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        for(int i = 0; i < N; i++) {
+        	choo[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        Arrays.sort(choo);
+        
+        int psum = 0;
+        
+        for(int i = 0; i < N; i++) {
+        	if(choo[i] <= psum + 1) 
+        		psum += choo[i];
+        	else 
+        		break;
+        }
+        
+        System.out.println(psum + 1);
+        
+    }
+    
+}
